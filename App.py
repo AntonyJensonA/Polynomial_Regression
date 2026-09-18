0bd1f66ee81ad3478f6e2f1e471fd183cf596db2
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-18 09:18:32 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
 import joblib
 
 model = joblib.load("Bill_model.pkl")
@@ -10,9 +11,11 @@ ac_unit = st.number_input(
 
 
 if st.button("Predict"):
-  input_data = pd.DataFrame({
-    "AC_Units": [ac_unit]
-  })
-  prediction = model.predict(input_data)
-  pred = prediction[0]
-  st.success(f"Predicted Price: ₹{pred:.2f}")
+    input_data = pd.DataFrame({
+        "AC_Units": [ac_unit]
+    })
+    poly = PolynomialFeatures(degree=2)
+    new_poly_df = poly.fit_transform(input_data)
+    prediction = model.predict(new_poly_df)
+    pred = prediction[0]
+    st.success(f"Predicted Price: ₹{pred:.2f}")
