bc35379b6dfd9f7f4d4a23996ecd65125df17dc7
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-18 10:40:32 +0530

    Update App.py

--- App.py
+++ App.py
@@ -6,7 +6,13 @@ import joblib
 model = joblib.load("Bill_model.pkl")
 st.title("Electricity Bill Prediction")
 ac_unit = st.number_input(
-    "Enter the AC unit consumption",
+    "Enter the AC's unit consumption",
+    min_value =1.0 ,
+    value =1.0,
+    step= 1.0
+)
+fan_unit = st.number_input(
+    "Enter the Fan's unit consumption",
     min_value =1.0 ,
     value =1.0,
     step= 1.0
@@ -16,10 +22,13 @@ ac_unit = st.number_input(
 if st.button("Predict"):
     if ac_unit < 1 or ac_unit > 150:
         st.error("AC units should be between 0 and 150 units")
+    elif fan_unit < 1 or fan_unit > 150:
+        st.error("Fan units should be between 0 and 150 units")
         
     else:
         input_data = pd.DataFrame({
-            "AC_Units": [ac_unit]
+            "AC_Units": [ac_unit],
+            "Fan_Units" :[fan_unit]
         })
         poly = PolynomialFeatures(degree=2)
         new_poly_df = poly.fit_transform(input_data)
