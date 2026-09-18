ddc0c5d4d0c4b8a6ae9d87a49cba9863baaa8731
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-18 09:35:05 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,15 +8,15 @@ st.title("Electricity Bill Prediction")
 ac_unit = st.number_input(
     "Enter the AC unit consumption",
     min_value =1.0 ,
-    max_value =15.0,
-    value =1.0)
+    value =1.0,
+    step= 1.0
+)
 
 
 if st.button("Predict"):
-    valid = True
     if ac_unit < 1 or ac_unit > 150:
         st.error("AC units should be between 0 and 150 units")
-        valid = False
+        
     if valid:
         input_data = pd.DataFrame({
             "AC_Units": [ac_unit]
