a2fd462110b35dd954a22bcee0f956089a47b388
AntonyJensonA <143073662+AntonyJensonA@users.noreply.github.com>
2026-09-18 09:11:24 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("Bill_Model.pkl")
+st.title("Electricity Bill Prediction")
+
+ac_unit = st.number_input(
+    "Enter the AC unit consumption")
+
+
+if st.button("Predict"):
+  input_data = pd.DataFrame({
+    "AC_Units": [ac_unit]
+  })
+  prediction = model.predict(input_data)
+  pred = prediction[0]
+  st.success(f"Predicted Price: ₹{pred:.2f}")
